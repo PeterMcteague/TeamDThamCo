bea8fb93163b2c69400fb758dcd9d2373d1290fb
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-11-04 17:10:39 +0000

    Revert "Added entity framework and models"
    This reverts commit 18b77d1b483ebd2fb87fa5be2634dfbb76e89fe8.

--- TeamDThamCo/TeamDThamCo/Models/Dispatch.cs
+++ TeamDThamCo/TeamDThamCo/Models/Dispatch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OrderService.Models
+namespace TeamDThamCo.Models
 {
     public class Dispatch
     {
--- TeamDThamCo/TeamDThamCo/Models/productOrder.cs
+++ TeamDThamCo/TeamDThamCo/Models/productOrder.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OrderService.Models
+namespace TeamDThamCo.Models
 {
     //Relational model between products and orders
-    public class ProductOrder
+    public class productOrder
     {
         public int id { get; set; }
 
