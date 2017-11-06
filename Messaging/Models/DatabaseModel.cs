b4dc87a0818f40a964393c41f2d1207d287738a9
Turnipmuncher <q5233000@live.tees.ac.uk>
2017-11-06 13:24:27 +0000

    Creating Database

--- Messaging/Models/DatabaseModel.cs
+++ Messaging/Models/DatabaseModel.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Messaging.Models
 {
-    public class DatabaseModel
+    public class MessagingContext : DbContext
     {
+        public MessagingContext(DbContextOptions<MessagingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public class Conversation
+        {
+            public int ConId { get; set; }
+            public string Topic { get; set; }
+            public string Url { get; set; }
+
+            public List<Message> Messages { get; set; }
+        }
+
+        public class Message
+        {
+            public int MessageId { get; set; }
+            public string UserId { get; }
+            public string Content { get; set; }
+            public string Topic { get; set; }
+            public Conversation Conversation { get; set; }
+        }
     }
 }
--- Messaging/Startup.cs
+++ Messaging/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Messaging.Models;
 
 namespace Messaging
 {
@@ -22,6 +24,9 @@ namespace Messaging
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=Messaging;Trusted_Connection=True;";
+            services.AddDbContext<MessagingContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
