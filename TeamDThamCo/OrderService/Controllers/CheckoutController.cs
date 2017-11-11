73641e50256fd1750c837108c2d7171df1887e60
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-11-11 15:10:05 +0000

    Scaffolded Checkout/Basket

---
+++ TeamDThamCo/OrderService/Controllers/CheckoutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // Post: Checkout/Index
+        [HttpPost]
+        public ActionResult Details(CheckoutItem id)
+        {
+            return View();
+        }
+    }
+}
---
+++ TeamDThamCo/OrderService/Models/CheckoutItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderService.Models
+{
+    public class CheckoutItem
+    {
+        int id { get; set; }
+        IEnumerable<Order> orders { get; set; }
+    }
+}
