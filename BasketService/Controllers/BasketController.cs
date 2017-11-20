05ecab1735f3ae4e6c81c025eb0d3e3a53b18311
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-11-20 12:16:42 +0000

    Updated XML comments

--- BasketService/Controllers/BasketController.cs
+++ BasketService/Controllers/BasketController.cs
@@ -39,12 +39,11 @@ namespace BasketService.Controllers
         /// <summary>
         /// Gets the basket of a customer.
         /// </summary>
-        /// <param name="User ID"></param>  
+        /// <param name="userid">The userid to get the basket of</param>  
         /// <response code="200">Returns the basket</response>
         /// <response code="400">If not any basket items by userid</response>  
         /// <response code="404">If parameters are invalid</response>
-        // GET: api/Basket/5
-        [HttpGet("{userid}", Name = "Get basket by buyer ID")]
+        [HttpGet("/{userid}", Name = "Get basket by buyer ID")]
         public async Task<IActionResult> GetBasket([FromRoute] string userid)
         {
             if (!ModelState.IsValid)
@@ -53,23 +52,26 @@ namespace BasketService.Controllers
             }
             if (!_context.Baskets.Any())
             {
-                return NotFound();
+                return NotFound("No baskets found");
             }
             var orders = await _context.Baskets.Where(b => b.buyerId == userid).ToListAsync();
             return Ok(orders);
         }
 
-        // Put: api/Basket/add/userId=5&productId=5&quantity=5
-        [HttpPut("add/userId={userId}&productId={productId}&quantity={quantity}", Name = "Add an item to a customers basket")]
+        /// <summary>
+        /// Adds a basket record. If the basket item already exists it adds to quantity.
+        /// </summary>
+        /// <param name="userId">The userId to add the item to.</param>  
+        /// <param name="productId">The productId to add to the basket.</param>  
+        /// <param name="quantity">The amount to add.</param>  
+        /// <response code="200">OK. Returns the item added.</response>
+        /// <response code="400">If parameters invalid.</response>  
+        [HttpPut("/add/userId={userId}&productId={productId}&quantity={quantity}", Name = "Add an item to a customers basket")]
         public async Task<IActionResult> AddItemToBasket([FromRoute] string userId, [FromRoute] int productId, [FromRoute] int quantity)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
-            }
-            if (!_context.Baskets.Any())
-            {
-                return NotFound();
             }
             else
             {
@@ -77,6 +79,7 @@ namespace BasketService.Controllers
                 {
                     var basketItem = _context.Baskets.FirstOrDefault(b => b.buyerId == userId && b.productId == productId);
                     basketItem.quantity = basketItem.quantity + quantity;
+                    _context.Update(basketItem);
                     await _context.SaveChangesAsync();
                     return Ok(basketItem);
                 }
@@ -90,30 +93,50 @@ namespace BasketService.Controllers
             }
         }
 
-        // Put: api/Basket/update/userId=5&productId=5&quantity=5
-        [HttpPut("update/userId={userId}&productId={productId}&quantity={quantity}", Name = "Update an items quantity a customers basket")]
+        /// <summary>
+        /// Updates a basket record.
+        /// </summary>
+        /// <param name="userId">The userId to add the item to.</param>  
+        /// <param name="productId">The productId to add to the basket.</param>  
+        /// <param name="quantity">The updated amount.</param>  
+        /// <response code="200">OK. Returns the item added.</response>
+        /// <response code="400">If parameters invalid.</response>
+        /// <response code="404">If basket item to update not found.</response>
+        [HttpPut("/update/userId={userId}&productId={productId}&quantity={quantity}", Name = "Update an items quantity a customers basket")]
         public async Task<IActionResult> UpdateItemInBasket([FromRoute] string userId, [FromRoute] int productId, [FromRoute] int quantity)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            if (!_context.Baskets.Any() || !_context.Baskets.Where(b => b.buyerId == userId && b.productId == productId).Any())
+            if (!_context.Baskets.Where(b => b.buyerId == userId && b.productId == productId).Any())
             {
-                return NotFound();
+                return NotFound("No item found with those arguments");
+            }
+            if (quantity == 0)
+            {
+                return BadRequest("Quantity is 0. Please use the delete method for this.");
             }
             else
             {
                 var basketItem = _context.Baskets.FirstOrDefault(b => b.buyerId == userId && b.productId == productId);
                 basketItem.quantity = quantity;
+                _context.Update(basketItem);
                 await _context.SaveChangesAsync();
                 return Ok(basketItem);
             }
         }
 
-        // DELETE: api/Basket/delete/userId=5&productId=5
-        [HttpDelete("delete/userId={userId}&productId={productId}")]
-        public async Task<IActionResult> DeleteBasketItem([FromRoute] string userId, [FromRoute] int productId)
+        /// <summary>
+        /// Deletes a basket item.
+        /// </summary>
+        /// <param name="userId">The userId to add the item to.</param>  
+        /// <param name="productId">The productId to add to the basket.</param>  
+        /// <response code="200">OK. Returns the item added.</response>
+        /// <response code="400">If parameters invalid.</response>
+        /// <response code="404">If item to delete not found.</response>
+        [HttpDelete("/delete/userId={userId}&productId={productId}")]
+        public async Task<IActionResult> DeleteBasketItem([FromRoute] string userId , [FromRoute] int productId)
         {
             if (!ModelState.IsValid)
             {
@@ -121,14 +144,14 @@ namespace BasketService.Controllers
             }
             if (!_context.Baskets.Any())
             {
-                return NotFound();
+                return NotFound("No baskets found");
             }
 
             var basketItem = await _context.Baskets.SingleOrDefaultAsync(m => m.productId == productId && m.buyerId == userId);
 
             if (basketItem == null)
             {
-                return NotFound();
+                return NotFound("No basket items found with those arguments");
             }
 
             _context.Baskets.Remove(basketItem);
--- TeamDThamCo/OrderService/Controllers/OrdersAPIController.cs
+++ TeamDThamCo/OrderService/Controllers/OrdersAPIController.cs
@@ -39,7 +39,7 @@ namespace OrderService.Controllers
             }
             if (!_context.Orders.AsNoTracking().Any())
             {
-                return NotFound();
+                return NotFound("No orders found");
             }
             if (!_context.Orders.AsNoTracking().Any(b => b.active == true))
             {
@@ -53,7 +53,7 @@ namespace OrderService.Controllers
         /// <summary>
         /// Gets all orders made by a customer.
         /// </summary>
-        /// <param name="Buyer ID"></param>  
+        /// <param name="buyerid">The buyer to get orders by.</param>  
         /// <response code="200">Returns the orders</response>
         /// <response code="400">If not any orders by buyerId</response>  
         /// <response code="404">If parameters are invalid</response>  
@@ -66,7 +66,7 @@ namespace OrderService.Controllers
             }
             if (!_context.Orders.Any())
             {
-                return NotFound();
+                return NotFound("No orders found");
             }
             if (!_context.Orders.Where(b => b.active == true && b.buyerId == buyerid).Any())
             {
@@ -91,7 +91,7 @@ namespace OrderService.Controllers
             }
             if (!_context.OrderItems.Any())
             {
-                return NotFound();
+                return NotFound("No orders found");
             }
 
             var products = await _context.OrderItems.AsNoTracking().Where(b => b.active == true).ToListAsync();
@@ -102,7 +102,7 @@ namespace OrderService.Controllers
         /// <summary>
         /// Gets all products in an order.
         /// </summary>
-        /// <param name="Order ID"></param>  
+        /// <param name="orderid">The ID of the order to get products by.</param>  
         /// <response code="200">Returns the products</response>
         /// <response code="400">If not any products with that orderID</response>  
         /// <response code="404">If parameters are invalid</response>  
@@ -115,7 +115,7 @@ namespace OrderService.Controllers
             }
             if (!_context.OrderItems.Any())
             {
-                return NotFound();
+                return NotFound("No orderitems found");
             }
             if (!_context.OrderItems.Where(b => b.active == true && b.orderId == orderid).Any())
             {
@@ -130,7 +130,7 @@ namespace OrderService.Controllers
         /// <summary>
         /// Gets all products ordered by a customer.
         /// </summary>
-        /// <param name="buyerid"></param>
+        /// <param name="buyerid">The buyerid to get order items by.</param>
         /// <response code="200">Returns the products</response>
         /// <response code="400">If not any products ordered by that buyer</response>  
         /// <response code="404">If parameters are invalid</response>  
@@ -143,11 +143,11 @@ namespace OrderService.Controllers
             }
             if (!_context.OrderItems.AsNoTracking().Any())
             {
-                return NotFound();
+                return NotFound("No orderitems found");
             }
             if (!_context.Orders.AsNoTracking().Any())
             {
-                return NotFound();
+                return NotFound("No orders found");
             }
             if (!_context.Orders.AsNoTracking().Any(b => b.active == true && b.buyerId == buyerid))
             {
@@ -169,7 +169,7 @@ namespace OrderService.Controllers
         /// <summary>
         /// Cancels an order if not dispatched.
         /// </summary>
-        /// <param name="Order ID"></param>  
+        /// <param name="orderid">The order to delete</param>  
         /// <response code="200">Successfully deleted</response>
         /// <response code="400">If not any orders with that orderID that aren't dispatched</response>  
         /// <response code="404">If parameters are invalid</response>  
@@ -182,7 +182,7 @@ namespace OrderService.Controllers
             }
             if (!_context.Orders.AsNoTracking().Any())
             {
-                return NotFound();
+                return NotFound("No orders found");
             }
             if (!_context.Orders.AsNoTracking().Any(b => b.active == true && b.id == orderid))
             {
@@ -196,7 +196,9 @@ namespace OrderService.Controllers
                 foreach (OrderItem product in products)
                 {
                     product.active = false;
+                    _context.Update(product);
                 }
+                _context.Update(order);
                 await _context.SaveChangesAsync();
                 var returnValue = _context.Orders.AsNoTracking().Where(b => b.active == true);
                 return Ok(returnValue);
@@ -204,14 +206,14 @@ namespace OrderService.Controllers
         }
 
         /// <summary>
-        /// Removes an item from an order.
+        /// Removes an item from an order by orderid and productId
         /// </summary>
-        /// <param name="Product ID"></param>  
+        /// <param name="id">The id of the product to delete from an order</param>  
         /// <response code="200">Succesfully deleted from order</response>
         /// <response code="400">If product isn't found in any orders or is already dispatched</response>  
         /// <response code="404">If parameters are invalid</response>  
         [HttpPut("Delete/productId={productId}", Name = "Delete product from order if not dispatched")]
-        public async Task<IActionResult> DeleteProductFromOrder([FromRoute] int productId)
+        public async Task<IActionResult> DeleteProductFromOrder([FromRoute] int id)
         {
             if (!ModelState.IsValid)
             {
@@ -219,27 +221,34 @@ namespace OrderService.Controllers
             }
             if (!_context.OrderItems.AsNoTracking().Any())
             {
-                return NotFound();
+                return NotFound("No orderitems found");
             }
-            if (!_context.OrderItems.AsNoTracking().Any(b => b.active == true && b.id == productId))
+            if (!_context.OrderItems.AsNoTracking().Any(b => b.active == true && b.id == id))
             {
-                return NoContent();
+                return NotFound("No orderitems found");
             }
 
             else
             {
-                var product = _context.OrderItems.SingleOrDefault(m => m.id == productId && m.active);
+                var product = _context.OrderItems.SingleOrDefault(m => m.id == id && m.active);
                 var order = _context.Orders.SingleOrDefault(m => m.id == product.orderId);
                 if (!order.dispatched)
                 {
                     product.active = false;
+                    _context.Update(product);
                     await _context.SaveChangesAsync();
                     return Ok(product);
                 }
-                return NotFound("Order already dispatched");
+                return Forbid("Order already dispatched");
             }
         }
 
+        /// <summary>
+        /// Adds an order. Note items must be added to the order seperately.
+        /// </summary>
+        /// <param name="buyerId">The id of the buyer</param>
+        /// <param name="address">Their address</param>
+        /// <returns>Returns the added item.</returns>
         [HttpPost("Add/buyerId={buyerId}&address={address}", Name = "Add an item to a customers basket")]
         public async Task<IActionResult> AddOrder([FromRoute] string buyerId, [FromRoute] string address)
         {
@@ -256,6 +265,14 @@ namespace OrderService.Controllers
             }
         }
 
+        /// <summary>
+        /// Adds an item to an order
+        /// </summary>
+        /// <param name="orderId">The id of the order to add to</param>
+        /// <param name="name">The name of the item to add</param>
+        /// <param name="quantity">The quantity of the item to add</param>
+        /// <param name="cost">The cost of the item to add</param>
+        /// <returns></returns>
         [HttpPost("Add/orderId={orderId}&name={name}&quantity={quantity}&cost={cost}", Name = "Add an item to a customers basket")]
         public async Task<IActionResult> AddOrderItem([FromRoute] int orderId, [FromRoute] string name, [FromRoute] int quantity, [FromRoute] double cost)
         {
